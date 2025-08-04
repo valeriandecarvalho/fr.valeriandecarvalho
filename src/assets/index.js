c64124f7ee8ccd69f212aee6fff42c0a4414044f
Valérian <valeriandc.pro@gmail.com>
2025-08-05 01:25:36 +0200

    v0.0.0

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import logo from "./logos/logo.png";
+
+import close from "./icons/close.svg";
+import menu from "./icons/menu.svg";
+
+import github from "./logos/company/github.svg";
+import linkedin from "./logos/company/linkedin.svg";
+
+export {
+    logo,
+
+    close,
+    menu,
+
+    github,
+    linkedin,
+}
---
+++ src/sections/body/Body.jsx
@@ -0,0 +1,15 @@
+const Body = () => {
+    return (
+        <main>
+            <div className="max-w-6xl mx-auto px-4">
+                <div className="flex items-center justify-center min-h-screen">
+                    <h1 className="text-6xl xl:text-8xl font-bold text-center text-text">
+                        À venir
+                    </h1>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Body;
